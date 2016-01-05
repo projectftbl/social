e42ea273ab277ec18ca4beb4d0ac4cacdeeb34b4
Johnny Hall <johnny@recipher.co.uk>
2016-01-05 10:15:34 +0000

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,12 @@
+var should = require('chai').should()
+  , sut = require('../lib').facebook;
+
+describe('Facebook', function() {
+  
+  before(function() {
+  });
+  
+  it('should', function() {
+  });
+  
+});
